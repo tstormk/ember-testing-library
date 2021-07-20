72fe98b88e4f1dc193841b83b0c17c9a1efc6191
Theodor Storm <tstormk@gmail.com>
2021-07-20 12:41:16 +0200

    Add missing repository info; Re-install ember-cli-typescript

--- tests/dummy/app/config/environment.d.ts
+++ tests/dummy/app/config/environment.d.ts
@@ -1,17 +1,14 @@
+export default config;
+
 /**
  * Type declarations for
- *    import config from './config/environment'
- *
- * For now these need to be managed by the developer
- * since different ember addons can materialize new entries.
+ *    import config from 'my-app/config/environment'
  */
 declare const config: {
-  environment: any;
+  environment: string;
   modulePrefix: string;
   podModulePrefix: string;
   locationType: string;
   rootURL: string;
-  APP: any;
+  APP: Record<string, unknown>;
 };
-
-export default config;
